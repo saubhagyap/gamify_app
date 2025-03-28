521f8f674dbd4e4df6c5f489dcbbe68ab851b830
saubhagyap <saubhagyapandey20@gmail.com>
2025-03-28 23:58:53 +0530

    Adding Featured games

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,10 +12,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(),
+      body: Stack(
+        children: <Widget>[
+          _featuredGameWidget(),
+        ],
+      ),
     );
+  }
+
+  Widget _featuredGameWidget() {
+    return SizedBox(
+        height: _deviceHeight * 0.50,
+        width: _deviceWidth,
+        child: ListView(
+          children: featuredGames.map((_game) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(_game.coverImage.url),
+                ),
+              ),
+            );
+          }).toList(),
+        ));
   }
 }
